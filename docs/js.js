8a4c0b0e08d8a2e1b86e5a1af9999989b91135c5
xincunhoujie <null>
2024-12-29 01:41:33 +0000

    🎉auto update by Gmeek action

--- docs/js.js
+++ docs/js.js
@@ -1,4 +1,4 @@
 let element = document.getElementById('footer'); // 根据容器的id获取元素
 //element.innerHTML += '<div class="xinhtml">这是新添加的HTML代码</div>'; // 在容器中添加新的HTML代码
 
-element.insertAdjacentHTML('beforebegin', '<div  class="hengfu"><a href="https://weiwei.sitesi.tc/" target="_blank"><img src=hf.gif" ></a></div>');
+element.insertAdjacentHTML('beforebegin', '<div  class="hengfu"><a href="https://weiwei.sitesi.tc/" target="_blank"><img src="hf.gif" ></a></div>');
