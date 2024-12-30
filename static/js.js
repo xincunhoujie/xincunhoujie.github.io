711e278f0bd452aaddbc5197c9b8b074dbb10f9d
黑仔 <xincunhoujie@hotmail.com>
2024-12-30 14:57:31 +0800

    Update js.js

--- static/js.js
+++ static/js.js
@@ -2,11 +2,13 @@ let element = document.getElementById('footer'); // 根据容器的id获取元�
 //element.innerHTML += '<div class="xinhtml">这是新添加的HTML代码</div>'; // 在容器中添加新的HTML代码
 
 element.insertAdjacentHTML('beforebegin', '<div  class="hengfu"><a href="https://weiwei.sitesi.tc/" target="_blank"><img src="/hf.gif" ></a></div>');
-/*获取视口，判断是否输出导航文字标签*/
+
+
+//获取视口，判断是否输出导航文字标签=======
 //const viewportWidth = document.body.clientWidth;
 const viewportWidth = window.innerWidth;
 //alert(viewportWidth);
-if ( viewportWidth < 600 ) {   
+if ( viewportWidth > 600 ) {   
     //获取图标导航的数量
     var title7 = document.getElementsByClassName("btn");
     //alert(title7.length);
@@ -16,3 +18,4 @@ if ( viewportWidth < 600 ) {
     title8.innerHTML += title8.getAttribute('title'); 
     }
  }
+//获取视口，判断是否输出导航文字标签=======结束
