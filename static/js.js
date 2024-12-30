82cb3e0b752a037c8efb06cda377e45ec439ea21
黑仔 <xincunhoujie@hotmail.com>
2024-12-30 10:03:47 +0800

    Update js.js

--- static/js.js
+++ static/js.js
@@ -2,3 +2,7 @@ let element = document.getElementById('footer'); // 根据容器的id获取元�
 //element.innerHTML += '<div class="xinhtml">这是新添加的HTML代码</div>'; // 在容器中添加新的HTML代码
 
 element.insertAdjacentHTML('beforebegin', '<div  class="hengfu"><a href="https://weiwei.sitesi.tc/" target="_blank"><img src="/hf.gif" ></a></div>');
+
+var title8 = document.getElementById('title-right a');
+//div.textContent = title8.getAttribute('title');
+title8.insertAdjacentHTML('beforebegin', title8.getAttribute('title'));
